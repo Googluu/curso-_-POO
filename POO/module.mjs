f8add4b2d43eba41bc11db59b2f76397f61f77db
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-03-03 14:29:28 -0600

    Procesando y guardando

---
+++ POO/module.mjs
@@ -0,0 +1,16 @@
+import { PlatziClass } from "./main.mjs";
+
+const clase67 = new PlatziClass({
+    name: "JavaScript: orientado a objetos, basados en prototipos",
+    videoID: "niidea;xdxdxd",
+});
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
